a78b1ec05f8aaa27d3d94d4b9c89a176532ee1c2
apathya <apathyasp@gmail.com>
2013-10-22 05:47:54 -0700

    Update HellowWorld.java

--- HellowWorld.java
+++ HellowWorld.java
@@ -1,10 +1,3 @@
-/**
- * Created with IntelliJ IDEA.
- * User: apathy
- * Date: 21.10.13
- * Time: 19:29
- * To change this template use File | Settings | File Templates.
- */
 public class HellowWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
